64de70319e836f6470b6e9de751e2976e9fa7ea5
Rebeca Sene <rebecabsene@gmail.com>
2026-05-22 17:33:36 -0300

    atualizações

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,7 +1,100 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
-  it('verifica o título da aplicação', () => {
+  beforeEach(() => {
     cy.visit('./src/index.html')
-
+  
+})
+  it('verifica o título da aplicação', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+  
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    cy.get('#firstName')
+    .type('Rebeca')
+
+    cy.get('#lastName')
+    .type('Sene')
+    
+    cy.get('#email')
+    .type('rebecabsene@gmail.com')
+
+    cy.get('#open-text-area')
+    .type('Teste, teste, teste oi.')
+
+    cy.get('.button')
+    .click()
+
+    cy.get('.success')
+    .should('be.visible')
+  })
+
+
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName')
+    .type('Rebeca')
+
+    cy.get('#lastName')
+    .type('Sene')
+    
+    cy.get('#email')
+    .type('rebecarebeca.com')
+
+    cy.get('#open-text-area')
+    .type('Teste, teste, teste oi.')
+
+    cy.get('.button')
+    .click()
+
+    cy.get('.error')
+    .should('be.visible')
+
+})
+
+  it('testa se campo do telefone fica vazio ao digitar letras', () => {
+    cy.get('#firstName')
+    .type('Rebeca')
+
+    cy.get('#lastName')
+    .type('Sene')
+    
+    cy.get('#email')
+    .type('rebecabsene@gmail.com')
+
+    cy.get('#phone')
+    .type('testetest')
+    .should('have.value', '')
+
+    cy.get('#open-text-area')
+    .type('Teste, teste, teste oi.')
+
+    cy.get('.button')
+    .click()
+
+  })
+
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName')
+    .type('Rebeca')
+
+    cy.get('#lastName')
+    .type('Sene')
+    
+    cy.get('#email')
+    .type('rebecabsene@gmail.com')
+
+    cy.get('#phone-checkbox')
+    .click()
+
+    cy.get('#phone')
+    .type(' ')
+
+    cy.get('#open-text-area')
+    .type('Teste, teste, teste oi.')
+
+    cy.get('.button')
+    .click()
+
+    cy.get('.error')
+    .should('be.visible')
+
   })
 })
