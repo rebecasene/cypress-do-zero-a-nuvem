5f3b6a5537589c98629dbd130e7ac33066dcdfe0
Rebeca Sene <rebecabsene@gmail.com>
2026-05-27 18:38:43 -0300

    atualizações

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -168,4 +168,39 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('.success')
     .should('be.visible')
   })
+
+  it('seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product')
+    .select('YouTube')
+    .should('have.value', 'youtube')
+
+  })
+
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+    cy.get('#product')
+    .select('mentoria')
+    .should('have.value', 'mentoria')
+  })
+
+  it('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product')
+    .select(1)
+    .should('have.value', 'blog')
+  })
+
+  it('marca o tipo de atendimento "Feedback"', () => {
+    cy.get('input[value="feedback"]')
+    .check()
+    .should('be.checked')
+  })
+
+  it.only('marca cada tipo de atendimento', () => {
+    cy.get('input[type="radio"]')
+    .each(typeOfService => {
+      cy.wrap(typeOfService)
+        .check()
+        .should('be.checked')
+
+    })
+  })
 })
